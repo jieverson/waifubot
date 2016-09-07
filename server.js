7d5f2487101add17d22a86edd6f7374abba9e254
Jiéverson Maissiat <jieverson@craftbox.com.br>
2016-09-06 23:24:51 -0300

    migrating to botbuilder 3

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const restify = require('restify')
-const bot = require('./bot/bot.js')
+const botConnector = require('./bot/connector.js')
 
 // Setup Restify Server
 const server = restify.createServer()
-server.post('/api/bot', bot.verifyBotFramework(), bot.listen())
 server.listen(process.env.port || 3978, () => {
     console.log('%s listening to %s', server.name, server.url) 
 })
+
+server.post('/api/bot', botConnector.listen())
